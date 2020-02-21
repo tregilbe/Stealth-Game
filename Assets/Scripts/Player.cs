9915592578eec4b26b5adc3e4b290e8e8840538b
Trenton <tregilbe@uat.edu>
2020-02-21 08:35:13 -0700

    Player movement and camera that follows the player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Transform tf;
+
+    public float rotationSpeed = 1;
+    public float movementSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            tf.position += tf.up * movementSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            tf.position += tf.up * -movementSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            tf.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            tf.Rotate(0, 0, -rotationSpeed * Time.deltaTime);
+        }
+
+    }
+}
