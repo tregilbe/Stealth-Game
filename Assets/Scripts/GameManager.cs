f0b7cccd0a08e8b02d3b839349ed89423e797b20
Trenton <tregilbe@uat.edu>
2020-02-24 21:06:27 -0700

    Title Screen finished

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject TitleBackgroundImage;
+    public GameObject TitleText;
+    public GameObject GameStartButton;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,17 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        TitleBackgroundImage.SetActive(false);
+        TitleText.SetActive(false);
+        GameStartButton.SetActive(false);
     }
 }
---
+++ Assets/Scripts/PercentageBar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PercentageBar : MonoBehaviour
+{
+    public float current = 100.0f;
+    public float max = 100.0f;
+    public Image BarImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BarImage = gameObject.GetComponent<Image>();
+        BarImage.type = Image.Type.Filled;
+        BarImage.fillMethod = Image.FillMethod.Horizontal;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float percentFilled = current / max;
+        BarImage.fillAmount = percentFilled;
+        if (percentFilled > 0.25f)
+        {
+            BarImage.color = Color.green;
+        }
+        else
+        {
+            BarImage.color = Color.red;
+        }
+    }
+}
